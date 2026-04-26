018c99752b273913902d81ee94a02085acfcdd13
nullPointerLuka <llucas@ucompensar.edu.co>
2026-04-26 18:46:47 -0500

    Update app.js

--- COMPONENTES/Home/app.js
+++ COMPONENTES/Home/app.js
@@ -6,11 +6,6 @@ let busquedaActiva = "";
 
 // FETCH
 
-fetch("https://nullpointerluka.github.io/Movix/COMPONENTES/BD/movies.json")
-  .then(res => res.text())
-  .then(text => {
-    console.log(text); 
-  });
 
 fetch("https://nullpointerluka.github.io/Movix/COMPONENTES/BD/movies.json")
   .then(response => response.json())
